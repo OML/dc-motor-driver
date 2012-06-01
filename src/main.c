2548b2335a68b91e09670de17d2592c04fdaa23a
Wouter Dijkstra <wouter-dijkstra@hotmail.com>
2012-06-01 09:58:23 +0200

    recieve packets re-implemented

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@
 
 #include <stdint.h>        /* Includes uint16_t definition                    */
 #include <stdbool.h>       /* Includes true/false definition                  */
+#include <stdlib.h>
+#include <string.h>
 
 #include "system.h"        /* System funct/params, like osc/peripheral config */
 
@@ -15,11 +17,80 @@
 #include <bus.h>
 #include <offsets.h>
 
+//volatile uint16_t addr;
+volatile uint16_t source;
+
+volatile uint8_t rx_buffer[128];
+volatile uint8_t rx_current = 0;
+volatile uint8_t rx_flags = 0;
+volatile uint8_t rx_length;
+
+volatile uint8_t tx_buffer[128];
+volatile uint8_t tx_current = 0;
+volatile uint8_t tx_size = 0;
+
 void incoming_event(struct bus_descriptor* bus, char* data, size_t len)
 {
         struct bus_event_hdr* evhdr = get_bus_event_header(data);
-        switch(evhdr->type) {
-        }
+        switch(evhdr->type) 
+		{
+	rx_buffer[rx_current] = U1RXREG;
+	rx_current++;
+
+	struct bus_hdr* header;
+	struct bus_event_hdr* event_hdr;
+	struct bus_set_motor_driver* set_driver;
+
+	if(rx_current == 2)
+	{
+		memcpy((uint8_t*)&rx_length, (uint8_t*)(rx_buffer), 2); // put the first 2 bytes into rx_length
+	}
+
+	if(rx_current == rx_length)
+	{
+		header = (struct bus_hdr*)(rx_buffer + sizeof(uint16_t));
+		if(header->opcode.op == BUSOP_HELLO)
+		{
+			memcpy((uint16_t*)&addr, (uint16_t*)header->daddr,2);
+			memcpy((uint16_t*)&source, (uint16_t*)header->saddr,2);
+		}
+
+		if(header->opcode.op == BUSOP_EVENT && header->daddr == addr)
+		{
+			event_hdr = (struct bus_event_hdr*)((char*)header + sizeof(header));
+
+			if(event_hdr->type == EV_SET_THROTTLES)
+			{
+				set_driver = (struct bus_set_motor_driver*)((char*)event_hdr + sizeof(event_hdr));
+
+				device.motor[0].throttle = abs(set_driver->motors[0]);
+				if(set_driver->motors[0] > 0)
+					device.motor[0].flags = MOT_FORWARD;
+				else if(set_driver->motors[0] < 0)
+					device.motor[0].flags = MOT_BACKWARD;
+				else
+					device.motor[0].flags = MOT_BRAKE;
+
+
+				device.motor[1].throttle = abs(set_driver->motors[0]);
+				if(set_driver->motors[1] > 0)
+					device.motor[1].flags = MOT_FORWARD;
+				else if(set_driver->motors[1] < 0)
+					device.motor[1].flags = MOT_BACKWARD;
+				else
+					device.motor[1].flags = MOT_BRAKE;
+
+				motor_set_power(1,set_driver->motors[0]/10);
+				motor_set_power(2,set_driver->motors[1]/10);
+			}
+		}
+
+		if(header->opcode.op == BUSOP_DONE)
+		{
+			//nothing?
+		}
+	}
+	}
 }
 
 int16_t main(void)
